87acbee3b9881ab01681cdfc626df0e8081f754b
Karol Koptyra <koptyrakarol@gmail.com>
2022-05-25 00:12:55 +0200

    Move repeated timer class to src

---
+++ src/repeated_timer.py
@@ -0,0 +1,26 @@
+from threading import Timer
+
+class RepeatedTimer:
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def __run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self.__run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
