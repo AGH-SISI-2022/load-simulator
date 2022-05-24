dacf2b9bf5a9acd662a6b6f8e5994c90c4a7aa8e
Karol Koptyra <koptyrakarol@gmail.com>
2022-05-25 00:13:19 +0200

    Create past data generator - not yet working

---
+++ src/past_data_generator.py
@@ -0,0 +1,13 @@
+from src.youtuber import YouTuber
+from src.video import Video
+
+class PastDataGenerator: # TODO
+    def __init__(self):
+        generation_date_start = 0
+        generation_date_end = 0
+
+        while generation_date_start < generation_date_end:
+            pass
+
+if __name__ == "__main__":
+    data_generator = PastDataGenerator()
