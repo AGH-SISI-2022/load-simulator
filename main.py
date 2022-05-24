bea98690179eb8c5e53ce199dfb4af00ae8f1eb8
Karol Koptyra <koptyrakarol@gmail.com>
2022-05-25 00:50:14 +0200

    Add data generation option to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import configparser
 from src.load_simulator import LoadSimulator
+from src.past_data_generator import PastDataGenerator
 
 if __name__ == '__main__':
     config_parser = configparser.ConfigParser()
@@ -7,4 +8,9 @@ if __name__ == '__main__':
     send_period = int(config_parser['requests']['send_period'])
     watch_period = int(config_parser['requests']['watch_period'])
     subscribers_mltpr = int(config_parser['requests']['subscribers_mltpr'])
-    load_simulator = LoadSimulator('http://127.0.0.1:8080', send_period, watch_period, subscribers_mltpr)
+
+    # generate old data
+    data_generator = PastDataGenerator((2020,1,1), (2020,1,2), send_period, watch_period, subscribers_mltpr)
+
+    # simulate load
+    # load_simulator = LoadSimulator('http://127.0.0.1:8080', send_period, watch_period, subscribers_mltpr)
