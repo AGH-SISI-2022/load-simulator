a853be3701b3f7c308af3f3811d33c4036387b4a
Karol Koptyra <koptyrakarol@gmail.com>
2022-05-25 19:58:10 +0200

    Add tags to csv generator

--- src/past_data_generator.py
+++ src/past_data_generator.py
@@ -1,25 +1,13 @@
 from datetime import datetime
 from math import gcd
 import csv
-from src.utils import get_random_youtuber, normal_distribution
-from src.youtuber import YouTuber
+from src.utils import get_random_youtuber, normal_distribution, youtubers
 from src.video import Video
 
 class PastDataGenerator: 
     def __init__(self, date_start, date_end, send_period, watch_period, subscribers_mltpr):
 
-        self.youtubers = [
-            YouTuber('PewDiePie', 100, 0.2, 20, 15),
-            YouTuber('Mark Rober', 40, 1.0, 20, 1),
-            YouTuber('RATIRL', 10, 0.9, 15, 16),
-            YouTuber('DIY Perks', 25, 0.95, 20, 1),
-            YouTuber('Linus Tech Tips', 60, 0.3, 10, 30),
-            YouTuber('Uwaga! Naukowy Bełkot', 15, 0.5, 18, 5),
-            YouTuber('Thebausffs', 10, 1.0, 11, 12),
-            YouTuber('LEC', 10, 0.5, 5, 10),
-            YouTuber('TikTok ABC', 30, 0.8, 1, 50),
-            YouTuber('ilmango', 20, 0.3, 10, 15),
-        ]
+        self.youtubers = youtubers
 
         for ytber in self.youtubers:
             ytber.subscribers *= subscribers_mltpr
@@ -33,7 +21,7 @@ class PastDataGenerator:
         step = gcd(send_period, watch_period)
 
         with open('data/requests_data.csv', 'w', newline='') as csvfile:
-            fieldnames = ['endpoint', 'current_time', 'title', 'video_time', 'upload_time', 'youtuber_username', 'subscribers', 'request_count']
+            fieldnames = ['endpoint', 'current_time', 'title', 'video_time', 'upload_time', 'youtuber_username', 'subscribers', 'tags', 'request_count']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
@@ -50,7 +38,7 @@ class PastDataGenerator:
 
                 if self.date_start % watch_period == 0:
                     for v in self.videos:
-                        views = v.get_views()
+                        views = v.get_views(self.date_start)
                         self.write_row(writer, '/watch', v, views)
                         
                 for (date_time, vid) in self.videos_to_remove:
@@ -69,5 +57,6 @@ class PastDataGenerator:
             'upload_time': v.upload_time,
             'youtuber_username': v.youtuber.username,
             'subscribers': v.youtuber.subscribers, 
+            'tags': [t.name for t in v.tags],
             'request_count': views
         })
