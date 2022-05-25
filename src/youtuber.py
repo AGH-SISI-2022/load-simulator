8264457813b20aca0cc14422ab22c2324fb19626
Karol Koptyra <koptyrakarol@gmail.com>
2022-05-25 19:55:40 +0200

    Add tags to YouTuber class and change __str__ representation

--- src/youtuber.py
+++ src/youtuber.py
@@ -14,16 +14,19 @@ class YouTuber:
         Average time of uploaded video by YouTuber
     upload_rate: int
         Average upload rate of videos - per month
+    video_tags: [(Tag, float), (Tag, float), ...]
+        Tags that YouTuber uses and their propabilities
     '''
-    def __init__(self, username: str, subscribers: int, real_subs: float, avg_video_time: int, upload_rate: int):
+    def __init__(self, username: str, subscribers: int, real_subs: float, avg_video_time: int, upload_rate: int, video_tags: list):
         self.username = username
         self.subscribers = subscribers
         self.real_subs = real_subs
         self.avg_video_time = avg_video_time
         self.upload_rate = upload_rate
+        self.video_tags = video_tags
 
     def __str__(self):
         return f'''YouTuber {self.username} - {self.subscribers} subscribers
-            {self.subscribers}% are real subscribers
-            Average video time {self.avg_video_time}
+            {self.real_subs * 100}% are real subscribers
+            Average video time {self.avg_video_time} min
             Averaging {self.upload_rate} videos a month'''
