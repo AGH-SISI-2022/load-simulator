2eebb986b7702db86780f37b7bd6f0aff34abea1
Karol Koptyra <koptyrakarol@gmail.com>
2022-06-27 15:04:13 +0200

    Measure response times

--- src/load_simulator.py
+++ src/load_simulator.py
@@ -1,5 +1,5 @@
-import grequests
-from threading import Timer
+import grequests, csv, time
+from threading import Timer, Thread
 from src.utils import normal_distribution, get_random_youtuber, generate_title, youtubers
 from src.video import Video
 from src.repeated_timer import RepeatedTimer
@@ -21,6 +21,20 @@ class LoadSimulator:
         self.videos = []
         self.send_timer = RepeatedTimer(send_period, self.__send_video)
         self.view_videos = RepeatedTimer(watch_period, self.__view_videos)
+        with open('resp_times.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['time', 'requests count', 'avg response time', 'min response time', 'max response time'])
+
+    def __get_response_times(self, rs, req_times) -> None:
+        for resp in grequests.imap(rs, size=10):
+            req_times.append(resp.elapsed.total_seconds())
+
+    def __get_response_time_stats(self, tm, req_times, req_threads):
+        for t in req_threads:
+            t.join()
+        with open('resp_times.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([tm, len(req_times), sum(req_times)/len(req_times), min(req_times), max(req_times)])
 
     def __send_video(self) -> None:
         # make video for one of ytbers
@@ -41,11 +55,21 @@ class LoadSimulator:
         Timer(max(20*self.time_mltpr, 3 * video.video_time), self.__remove_video, [video]).start()
 
     def __view_videos(self) -> None:
+        if not self.videos:
+            return
+
+        req_times = []
+        req_threads = []
+        tm = int(time.time())
         for v in self.videos:
             views = v.get_views()
             print(f'Sending {views} view requests for video from {v.youtuber.username}')
             rs = (grequests.post(f'{self.ip}/watch', data=v.get_video_information()) for _ in range(views))
-            grequests.map(rs)
+            t = Thread(target=self.__get_response_times, args=(rs,req_times,))
+            t.start()
+            req_threads.append(t)
+
+        Thread(target=self.__get_response_time_stats, args=(tm, req_times, req_threads, )).start()
 
     def __remove_video(self, video: Video) -> None:
         try:
